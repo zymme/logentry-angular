11a03dc7dfa07a745d568851760df9f871384079
Dave Zimmer <dave.zimmer@viawest.com>
2018-09-05 08:11:15 -0500

    added call to create logentry

---
+++ src/app/logentry/ILogEntry.ts
@@ -0,0 +1,6 @@
+export interface ILogEntry {
+    activity: string,
+    notes: string,
+    duration: string,
+    
+}
--- src/app/services/log-entry.service.ts
+++ src/app/services/log-entry.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse} from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { catchError } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { throwError } from "rxjs";
+
+import { HttpErrorResponse } from "@angular/common/http/src/response";
+import { ILogEntry } from '../logentry/ILogEntry';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogEntryService {
+  private _entryApi: string;
 
-  constructor() { }
+  constructor(private _http: HttpClient) { 
+    this._entryApi = environment.logEntryApi;
+
+  }
+
+  createLogEntry(logentry: ILogEntry) : Observable<ILogEntry> {
+
+    try {
+      console.log(`Entered createLogEntry with ${logentry}`);
+      
+      return this._http.post<ILogEntry>(this._entryApi, logentry)
+        .pipe( 
+          tap(resp => console.log(JSON.stringify(resp))
+        ), 
+        catchError(this.handleError)
+    );
+
+    }
+    catch(error){
+      console.error("ERROR in createLogEntry: " + error);
+      return null;
+    }
+    
+  }
+
+  private handleError(err: HttpErrorResponse)  {
+    console.log('handleError received ' + err.message);
+    return Observable.throw(err);
+  }
 }
