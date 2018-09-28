2a2b6418f6d6bef342546ed38c2f8e65cbccf6ab
Dave Zimmer <dave.zimmer@viawest.com>
2018-09-28 08:14:00 -0500

    added in token inspection

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-main',
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private _oauthService: OAuthService) { 
+
+    console.log("Inside MainComponent ctor...");
+
+    var token = this._oauthService.getAccessToken();
+    console.log(`token: ${token}`);
+
+    debugger;
+
+    var claims = this._oauthService.getIdentityClaims();
+    console.log(`claims for this user: ${claims}`);
+
+    let arr = [];
+
+    for(let k in claims)
+    {
+      console.log(k);
+      console.log(claims[k]);
+    }
+
+  }
 
   ngOnInit() {
   }
